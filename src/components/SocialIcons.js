dc790e8ee4237f4d41a6ae8697079483a865aa79
mayowa86 <mayowasamuel86@gmail.com>
2022-03-10 07:58:14 +0100

    setting up

---
+++ src/components/SocialIcons.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {BsTwitter, BsFacebook, BsLinkedin} from 'react-icons/bs'
+import { StyledSocialIcons } from './styles/SocialIcons.style'
+
+const SocialIcons = () => {
+  return (
+    <StyledSocialIcons>
+        <li>
+        <a href='https://twitter.com'>
+          <BsTwitter />
+        </a>
+      </li>
+      <li>
+        <a href='https://facebook.com'>
+          <BsFacebook />
+        </a>
+      </li>
+      <li>
+        <a href='https://linkedin.com'>
+          <BsLinkedin />
+        </a>
+      </li>        
+    </StyledSocialIcons>
+  )
+}
+
+export default SocialIcons
